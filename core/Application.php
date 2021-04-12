ced3758191454535dbb7782c79ad5a760a553548
Dara <dara-@outlook.jp>
2021-04-12 05:47:03 +0000

    basic routing skelly

---
+++ core/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+// @package app\core
+
+namespace app\core;
+
+class Application {
+	public Router $router;
+	public Request $request;
+
+	public function __construct() {
+		$this->request = new Request();
+		$this->router = new Router($this->request);
+	}
+
+	public function run() {
+		$this->router->resolve();
+	}
+}
