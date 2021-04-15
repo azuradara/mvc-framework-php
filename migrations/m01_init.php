aec76bf2c9e9fcbe2b08f384e24a3bd95412b0a0
Aymane D. Hlamnach <dara-@outlook.jp>
2021-04-15 15:24:33 +0000

    migration eg

--- migrations/m01_init.php
+++ migrations/m01_init.php
@@ -3,11 +3,24 @@
 class m01_init {
 	public function export()
 	{
-		echo 'Exporting..';
+		$db = \app\core\Application::$app->db;
+		$sql = '		
+		create table user (
+			userId serial primary key,
+			userUsername varchar(16) not null,
+			userEmail varchar(64) not null,
+			userPwd varchar(64) not null
+		);
+		';
+		$db->driver->exec($sql);
 	}
 
-	public function import()
+	public function drop()
 	{
-		echo 'Importing';
+		$db = \app\core\Application::$app->db;
+		$sql = '		
+		drop table if exists user;
+		';
+		$db->driver->exec($sql);
 	}
 }
